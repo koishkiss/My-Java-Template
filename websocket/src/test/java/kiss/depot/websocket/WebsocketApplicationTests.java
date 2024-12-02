2bd45ae9eaaa2064f600ed1de467c9b74b7d78f0
koishikiss <202300300074@mail.sdu.edu.cn>
2024-12-02 22:12:05 +0800

    创建websocket模版库

---
+++ websocket/src/test/java/kiss/depot/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package kiss.depot.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
