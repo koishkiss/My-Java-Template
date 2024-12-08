5d03cea9f135f37158a59431541560abddd715e8
koishikiss <202300300074@mail.sdu.edu.cn>
2024-12-08 23:06:58 +0800

    更新wsUtil

--- websocket/src/main/java/kiss/depot/websocket/service/WebsocketService.java
+++ websocket/src/main/java/kiss/depot/websocket/service/WebsocketService.java
@@ -1,7 +1,21 @@
 package kiss.depot.websocket.service;
 
+import kiss.depot.websocket.model.dto.request.WsRequest;
+import kiss.depot.websocket.model.vo.response.WsResponse;
 import org.springframework.stereotype.Service;
+
+/*
+* ws消息处理
+* author: koishikiss
+* launch: 2024/12/8
+* last update: 2024/12/8
+* */
 
 @Service
 public class WebsocketService {
+
+    public WsResponse test(WsRequest<String> request) {
+        return WsResponse.success(request.getPath(),request.getMessage());
+    }
+
 }
--- websocket/src/main/java/kiss/depot/websocket/util/WebsocketUtil.java
+++ websocket/src/main/java/kiss/depot/websocket/util/WebsocketUtil.java
@@ -1,4 +1,68 @@
 package kiss.depot.websocket.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import kiss.depot.websocket.model.constant.STATIC;
+import kiss.depot.websocket.model.vo.response.WsResponse;
+import kiss.depot.websocket.service.WebsocketService;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+* ws相关操作
+* 包括session池、发送消息操作等
+* author: koishikiss
+* launch: 2024/12/8
+* last update: 2024/12/8
+* */
+
 public class WebsocketUtil {
+
+    // websocket session池
+    public static final Map<String, WebSocketSession> SESSION_MAP = new HashMap<>();
+
+    // websocket处理层
+    private static final WebsocketService websocketService = new WebsocketService();
+
+    /**
+     * 解析接收到的消息并转发给service层处理
+     */
+    public static void handleRoute(String payload, String uid) {
+        try {
+            sendOneMessage(websocketService.test(STATIC.objectMapper.readValue(payload, new TypeReference<>() {})), uid);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 向单个用户发送一条消息
+     */
+    public static void sendOneMessage(WsResponse response, String uid) {
+        try {
+            SESSION_MAP.get(uid).sendMessage(new TextMessage(STATIC.objectMapper.writeValueAsString(response)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 向多个用户发送同一消息
+     */
+    public static void sendMoreMessage(WsResponse response, String... uidList) {
+        try {
+            TextMessage textMessage = new TextMessage(STATIC.objectMapper.writeValueAsString(response));
+            for (String uid : uidList) {
+                SESSION_MAP.get(uid).sendMessage(textMessage);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
